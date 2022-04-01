fce3ece049590a1867ba5908bfb32ccf777e5b30
b301 <98285034+b301@users.noreply.github.com>
2022-04-02 00:23:34 +0300

    Update dirtree.py

--- lingui/dirtree.py
+++ lingui/dirtree.py
@@ -1,24 +1,43 @@
 import tkinter
+from tkinter import font
 
 
-DIRECTORIES = ["/home", "/var", "/bin", "/root", "/etc", "/usr"]
+DIRECTORIES = ["/home", "/var", "/bin", "/root", "/etc", "/usr", "/averylongdirectoryname"]
+BUTTON_HEIGHT = 2
+BUTTON_WIDTH = 16
 
 
 def main() -> None:
     root = tkinter.Tk()
-
-    buttons = create_buttons(root, DIRECTORIES)
+    _font = font.Font(family="Fira Code", size=10)
+    
+    buttons = create_buttons(root, _font)
 
     tkinter.Button(master=root, text="Delete all...", command=lambda x = buttons: delete_buttons(x)).pack()
 
     root.mainloop()
 
 
-def create_buttons(master: tkinter.Frame, directories: list) -> list:
+def create_buttons(master: tkinter.Frame, _font: font.Font) -> list:
+    directories = []
+    
     buttons = []
-    for directory_name in directories:
-        directory_button = tkinter.Button(master=master, text=directory_name, command=lambda x = directory_name: print(x))
-        directory_button.pack()
+    x = 0
+    y = 0
+    for directory_name, type in directories:
+        if type == 'd':
+            color = "green"
+        else:
+            color = "black"
+
+        directory_button = tkinter.Button(master=master, text=directory_name, command=lambda x = directory_name: print(x), width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg=color, font=_font)
+        directory_button.place(x=8.5*x, y=28*y)
+        
+        x += BUTTON_WIDTH
+        if x == 4 * BUTTON_WIDTH:
+            x = 0
+            y += BUTTON_HEIGHT
+
         buttons.append(directory_button)
     
     print(">>> Created Buttons")
@@ -27,12 +46,25 @@ def create_buttons(master: tkinter.Frame, directories: list) -> list:
 
 def delete_buttons(buttons: list) -> None:
     for button in buttons:
-        print("beep")
         button.destroy()
-        print("bop")
 
     print(">>> Deleted buttons")
     return
+
+
+
+def ls_to_list(ls: str) -> list:
+    files = ls.split('\n')[1:]
+
+    _files = []
+    for file in files:
+        attributes = file.split()
+        if 'd' in attributes[0]:
+            _files.append((attributes[-1], 'd'))
+        else:
+            _files.append((attributes[-1], 'f'))
+
+    return _files
 
 
 if __name__ == "__main__":

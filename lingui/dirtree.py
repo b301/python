a7ca370d3fa30f30e94656c4f034f9dc6b585281
b301 <98285034+b301@users.noreply.github.com>
2022-04-01 15:20:19 +0300

    Create dirtree.py

---
+++ lingui/dirtree.py
@@ -0,0 +1,39 @@
+import tkinter
+
+
+DIRECTORIES = ["/home", "/etc", "/bin"]
+
+
+def main() -> None:
+    root = tkinter.Tk()
+
+    buttons = create_buttons(root, ["/home", "/var", "/bin", "/root", "/etc", "/usr"])
+
+    tkinter.Button(master=root, text="Delete all...", command=lambda x = buttons: delete_buttons(x)).pack()
+
+    root.mainloop()
+
+
+def create_buttons(master: tkinter.Frame, directories: list) -> list:
+    buttons = []
+    for directory_name in directories:
+        directory_button = tkinter.Button(master=master, text=directory_name, command=lambda x = directory_name: print(x))
+        directory_button.pack()
+        buttons.append(directory_button)
+    
+    print(">>> Created Buttons")
+    return buttons
+
+
+def delete_buttons(buttons: list) -> None:
+    for button in buttons:
+        print("beep")
+        button.destroy()
+        print("bop")
+
+    print(">>> Deleted buttons")
+    return
+
+
+if __name__ == "__main__":
+    main()

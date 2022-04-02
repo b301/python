7edbf738d7fc4a580e4c3c519424438c51fc6224
b301 <98285034+b301@users.noreply.github.com>
2022-04-02 19:27:20 +0300

    Update navigation-sys.py

--- lingui/navigation-sys.py
+++ lingui/navigation-sys.py
@@ -5,67 +5,75 @@
 # __student__ >> b406
 #
 # todo:
-# frame to scroll in
+#
 
 
 import tkinter
-from tkinter import messagebox
 
 
 BUTTON_HEIGHT = 50
 BUTTON_WIDTH = 130
+BUTTONS = []
 
 
 def main() -> None:
     root = tkinter.Tk()
 
-    buttons = create_buttons(root)
+    # call the delete_buttons() and then create_buttons() after a cd ...
+    # create the buttons [based at ~ (f"/home/{USER}")
 
-    tkinter.Button(master=root, text="Delete all...", command=lambda x = buttons: delete_buttons(x)).pack()
+    # change directory >> delete_buttons(), change the directory, ls and then call ls_to_list() on the
+    # output. then call create_buttons on the frame, files
+    # should be good to go from here :D
+    #
+    # when you click on a file, you should call cat on the file and read it, redirect the output to
+    # read_file() which will open a popup window with the contents.
 
     root.mainloop()
 
 
-def create_buttons(master: tkinter.Frame) -> list:
+def create_buttons(master: tkinter.Frame, ls: str) -> None:
+    global BUTTONS
+    delete_buttons(BUTTONS)
     _FILE_ICON = tkinter.PhotoImage(file=".\\file.ico")
     _DIRECTORY_ICON = tkinter.PhotoImage(file=".\\directory.ico")
     FILE_ICON = _FILE_ICON.subsample(3, 3)
     DIRECTORY_ICON = _DIRECTORY_ICON.subsample(3, 3)
 
-    with open(".\\ls.txt", 'r') as f:
-        data = f.read()
-        directories = ls_to_list(data)
-    
+    # todo: fix the parameter to the output of a ls -la after a cd 
+    _files = ls_to_list(ls)
+
     buttons = []
     x = 0
     y = 0
-    for directory_name, type in directories:
+    for directory_name, type, perm in _files:
         if type == 'd':
-            directory_button = tkinter.Button(master=master, image=DIRECTORY_ICON, text=directory_name,command=lambda x = directory_name: print(x),
-             width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg="green", compound="top")
+            directory_button = tkinter.Button(master=master, image=DIRECTORY_ICON,
+             text=directory_name,command=lambda x = directory_name: print(x),
+              width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg="purple", compound="top")
             directory_button.image = DIRECTORY_ICON
         else:
-            directory_button = tkinter.Button(master=master, image=FILE_ICON, text=directory_name, command=lambda x = directory_name: read_file(master,x),
-             width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg="black", compound="top")
+            directory_button = tkinter.Button(master=master, image=FILE_ICON,
+             text=directory_name, command=lambda x = directory_name: read_file(master, x),
+              width=BUTTON_WIDTH, height=BUTTON_HEIGHT, fg="red", compound="top")
             directory_button.image = FILE_ICON
-        directory_button.place(x=1.3*x, y=1.3*y)
+        directory_button.place(x=1.05*x, y=1.1*y)
         
         x += BUTTON_WIDTH
-        if x == 4 * BUTTON_WIDTH:
+        if x == 6 * BUTTON_WIDTH:
             x = 0
             y += BUTTON_HEIGHT
 
         buttons.append(directory_button)
     
-    print("APP >>> Created Buttons")
-    return buttons
+    BUTTONS = buttons
+    return
 
 
 def delete_buttons(buttons: list) -> None:
     for button in buttons:
         button.destroy()
 
-    print("APP >>> Deleted buttons")
     return
 
 
@@ -76,17 +84,17 @@ def ls_to_list(ls: str) -> list:
     for file in files:
         attributes = file.split()
         if 'd' in attributes[0]:
-            _files.append((attributes[-1], 'd'))
+            _files.append((attributes[-1], 'd', True if attributes[0][7] == 'r' else False))
         else:
-            _files.append((attributes[-1], 'f'))
+            _files.append((attributes[-1], 'f', True if attributes[0][7] == 'r' else False))
 
     return _files
 
 
 def read_file(master: tkinter.Tk, title: str, content: str) -> None:
-    _POP_WIDTH = 400
-    _POP_HEIGHT = 600
-    
+    _POP_WIDTH = 1080
+    _POP_HEIGHT = 720
+
     pop_up = tkinter.Toplevel(master)
     pop_up.geometry(f"{_POP_WIDTH}x{_POP_HEIGHT}")
     pop_up.title(title)
@@ -96,7 +104,7 @@ def read_file(master: tkinter.Tk, title: str, content: str) -> None:
     text.insert(tkinter.END, content)
     text.config(state="disabled")
     text.pack()
-
+    
     return
 
 
